f53ab2b7267056e3099aefc112fed1b4dd312ce5
Roger Lam <roger@ordify.ai>
2025-03-21 20:39:11 -0500

    Updated with react basic

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Learn Game</h1>
+        <p>Welcome to my Firebase + React app!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
